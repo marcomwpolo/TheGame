40dc954d5785b6ac096af3dcf1f7ceab9e937ccb
gaving-quadprep <gaving@quadprep.org>
2024-10-10 11:03:14 -0400

    started adding a score system

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import pygame, sys, random
 # General setup
 pygame.init()
 clock = pygame.time.Clock()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 30)
+player_score = 0
+opponent_score = 0
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, player_score, opponent_score
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
@@ -13,7 +17,6 @@ def ball_animation():
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
         ball_restart()
-
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
 
@@ -48,6 +51,8 @@ pygame.display.set_caption('Pong')
 game_state = 1
 
 # Game rectangles
+opponent_score_text = my_font.render(str(opponent_score), False, (255, 255, 255))
+player_score_text = my_font.render(str(player_score), False, (255, 255, 255))
 ball = pygame.FRect(screen_width/2 - 15,screen_height/2 - 15,30.0,30.0)
 player = pygame.FRect(screen_width - 20, screen_height/2 - 70, 10.0,140.0)
 opponent = pygame.FRect(10.0, screen_height/2 - 70, 10.0, 140.0)
@@ -64,6 +69,10 @@ opponent_speed = 7
 while True:
     # Handling input
     if game_state == 0:
+        if ball.left <= 0:
+            opponent_score += 1
+        if ball.right >= screen.width:
+            player_score += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -89,6 +98,8 @@ while True:
         pygame.draw.rect(screen,light_grey, opponent)
         pygame.draw.ellipse(screen,light_grey, ball)
         pygame.draw.aaline(screen,light_grey, (screen_width/2,0), (screen_width/2,screen_height))
+        screen.blit(opponent_score_text, (screen_width/2-20, 0))
+        screen.blit(player_score_text, (screen_width/2+10, 0))
 
     if game_state == 1:
         pygame.draw.rect(screen,light_grey, rectangle)
